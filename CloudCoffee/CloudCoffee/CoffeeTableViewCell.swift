b4e417f6f5c127f6fefeee5798b51acd2ead8f00
OsnielLopes <niellt@hotmail.com>
2017-10-04 17:08:04 -0300

    Agora nós temos as classes das células dinâmicas

---
+++ CloudCoffee/CloudCoffee/CoffeeTableViewCell.swift
@@ -0,0 +1,29 @@
+//
+//  CoffeeTableViewCell.swift
+//  CloudCoffee
+//
+//  Created by Osniel Lopes Teixeira on 04/10/2017.
+//  Copyright © 2017 Nicholas Babo. All rights reserved.
+//
+
+import UIKit
+
+class CoffeeTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var coffeNameLabel: UILabel!
+    @IBOutlet weak var placeLabel: UILabel!
+    @IBOutlet weak var priceLabel: UILabel!
+    @IBOutlet weak var ratingLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
